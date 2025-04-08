9840d21d06f0ace120a21ab523f2b10fdb74bea1
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-04-08 14:40:35 +0200

    geometry changes

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -54,7 +54,7 @@ export default function Particles() {
     displacementTexture.needsUpdate = true;
   });
 
-  const geometry = new THREE.PlaneGeometry(10, 10, 526, 526);
+  const geometry = new THREE.PlaneGeometry(10, 10, 356, 356);
   geometry.setIndex(null);
   geometry.deleteAttribute('normal');
 
